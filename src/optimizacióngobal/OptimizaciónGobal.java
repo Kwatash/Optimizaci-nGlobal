372511e75c16a823aec66524699f4f4674251165
bko <bko@DESKTOP-NIMHIF7>
2022-11-22 17:32:07 -0600

    Programa 3 Optimizacion Global

---
+++ src/optimizacióngobal/OptimizaciónGobal.java
@@ -0,0 +1,28 @@
+package optimizacióngobal;
+import java.util.Scanner;
+public class OptimizaciónGobal {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero1,numero2;
+        System.out.print("Escriba el segundo entero: "); numero2 = entrada.nextInt();
+        System.out.print("Escriba el primer entero: "); numero1 = entrada.nextInt();
+        if(numero1==numero2)
+            System.out.printf("%d==%d\n", numero1,numero2);
+        if(numero1!=numero2)
+            System.out.printf(numero1+"!="+numero2);
+            System.out.printf("\n");
+        if(numero1<numero2)
+            System.out.printf("%d<%d\n", numero1,numero2);
+        if(numero1>numero2)
+            System.out.printf(numero1+">"+numero2);
+        System.out.printf("\n");
+        if(numero1<=numero2)
+            System.out.printf("%d<=%d\n", numero1,numero2);
+        if(numero1>=numero2)
+            System.out.printf(numero1+">="+numero2);
+            System.out.printf("\n");
+        if(numero1==numero2)
+            System.out.printf("%d==%d\n", numero1,numero2);
+    }
+}
